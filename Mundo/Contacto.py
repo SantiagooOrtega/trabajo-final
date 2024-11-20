fd4d5606ba362ebbd41ad53ab44ca9ec863a551e
juan jose <jjcalvache@gmail.com>
2024-11-20 11:50:59 -0500

    agregar trabajo inicial

---
+++ Mundo/Contacto.py
@@ -0,0 +1,30 @@
+class Contacto:
+
+    def __init__(self, nombre, apellido, direccion, correo):
+        
+
+    def darNombre(self):
+
+    def darApellido(self):
+
+    def darDireccion(self):
+
+    def darCorreo(self):
+
+    def darTelefonos(self):
+
+    def darPalabras(self):
+
+    def cambiarDireccion(self, direccion):
+
+    def cambiarCorreo(self, correo):
+
+    def agregarTelefono(self, telefono):
+
+    def eliminarTelefono(self, telefonoEliminar):
+
+    def agregarPalabra(self, palabra):
+
+    def eliminarPalabra(self, palabraEliminar):
+
+    def contienePalabraClave(self, palabra):
