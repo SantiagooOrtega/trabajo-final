3ec43ffa60bee98b6cb7c52617da654aca0c184a
juan jose <jjcalvache@gmail.com>
2024-11-20 12:58:43 -0500

    cree contacto y parte de lista de contactos

--- Mundo/Contacto.py
+++ Mundo/Contacto.py
@@ -1,30 +1,53 @@
 class Contacto:
 
     def __init__(self, nombre, apellido, direccion, correo):
-        
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__direccion = direccion
+        self.__correo = correo
+
+        self.__telefonos = [] 
+        self.__palabras = []
+
 
     def darNombre(self):
+        return self.__nombre
 
     def darApellido(self):
+        return self.__apellido
 
     def darDireccion(self):
+        return self.__direccion
 
     def darCorreo(self):
+        return self.__correo
 
     def darTelefonos(self):
+        return self.__telefonos
 
     def darPalabras(self):
+        return self.__palabras
 
     def cambiarDireccion(self, direccion):
+        self.__direccion = direccion
 
     def cambiarCorreo(self, correo):
+        self.__correo = correo
 
     def agregarTelefono(self, telefono):
+        self.__telefonos += [telefono]
 
     def eliminarTelefono(self, telefonoEliminar):
+        self.__telefonos.remove(telefonoEliminar)
 
     def agregarPalabra(self, palabra):
+        self.__palabras += [palabra]
 
     def eliminarPalabra(self, palabraEliminar):
+        self.__palabras.remove(palabraEliminar)
 
     def contienePalabraClave(self, palabra):
+        if palabra in self.__palabras:
+            return True
+        else:
+            return False
--- Mundo/ListaDeContactos.py
+++ Mundo/ListaDeContactos.py
@@ -2,14 +2,36 @@ from Mundo.Contacto import Contacto
 
 class ListaDeContactos:
     def __init__(self):
+        self.__contactos = []
 
     def darTodosLosContactos(self):
+        return self.__contactos
 
     def buscarContactosPalabraClave(self, palabra):
+        contactosConLaPalabra = []
+        for contacto in self.__contactos:
+            if contacto.contienePalabraClave(palabra):
+                contactosConLaPalabra += [contacto.darNombre()+" "+contacto.darApellido()]
+        return contactosConLaPalabra
+                
     
     def buscarContacto(self, nombre, apellido):
+        for contacto in self.__contactos:
+            if contacto.darNombre() == nombre and contacto.darApellido() == apellido:
+                return contacto
+        return None
 
     def agregarContacto(self, nombre, apellido, direccion, correo, telefonos, palabras):
+        if self.buscarContacto(nombre, apellido) == None:
+            ContactoNuevo = Contacto(nombre, apellido, direccion, correo)
+            for telefono in telefonos:
+                ContactoNuevo.agregarTelefono(telefono)
+            for palabra in palabras:
+                ContactoNuevo.agregarPalabra(palabra)   
+            self.__contactos += [ContactoNuevo]
+            return True
+        else:
+            return False
 
     def eliminarContacto(self, nombre, apellido):
 
